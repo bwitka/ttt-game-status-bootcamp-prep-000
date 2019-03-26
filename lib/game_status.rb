6192c37a2f0c3025b4a0f2afdbd8774a4638d736
BEAU WITKA <bwitka@gmail.com>
2019-03-26 19:03:34 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,7 +17,8 @@ WIN_COMBINATIONS = [
 
 def won?(board) 
   WIN_COMBINATIONS.each do |wincombo| 
-    if (board[wincombo[0]]) == "X" && (board[wincombo[1]]) == "X" && (board[wincombo[2]]) == "X" return wincombo 
+    if (board[wincombo[0]]) == "X" && (board[wincombo[1]]) == "X" && (board[wincombo[2]]) == "X" 
+      return wincombo 
     elsif (board[wincombo[0]]) == "O" && (board[wincombo[1]]) == "O" && (board[wincombo[2]]) == "O" 
     return wincombo 
     end 
