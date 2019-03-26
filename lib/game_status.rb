4d8bea89a1ff70dcf409065bc232f828c3162828
BEAU WITKA <bwitka@gmail.com>
2019-03-26 19:30:45 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -43,7 +43,7 @@ def over?(board)
 end
 
 def winner(board)
-  if won?(board) == ["X", "X", "X"]
+  if won?(board).count("X") == 3
     return "X"
   elsif  won?(board) == ["O", "O", "O"]
     return "O"
