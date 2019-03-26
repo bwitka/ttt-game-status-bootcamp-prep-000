c3acdad9e614ed26ce816ddf7ef5d5c761bc60a3
BEAU WITKA <bwitka@gmail.com>
2019-03-26 19:12:55 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,10 +18,10 @@ WIN_COMBINATIONS = [
 def won?(board) 
   WIN_COMBINATIONS.each do |win_combination| 
     if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X" || board[win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O"
-      return win_combination
+      return win_combination # return winning combo.
     end 
   end 
-    return false 
+    return false # return false if there is no win combo present in board.
 end
 
 def full?(board)
